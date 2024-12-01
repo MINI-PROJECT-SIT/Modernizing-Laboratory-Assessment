4970448461a7eb0af41330527389223f81643a85
Amogh <as7636688@gmail.com>
2024-12-01 17:35:55 +0530

    Added DB Schemas

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,23 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { mongoUrl } = require("../config");
 
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log("Connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(mongoUrl);
 
+//User Schema
 const userSchema = new Schema({
   username: {
-    //USN
+    type: String,
+    required: true,
+  },
+  usn: {
     type: String,
     required: true,
   },
   password: {
-    //DOB
     type: String,
     required: true,
   },
@@ -36,9 +32,13 @@ const userSchema = new Schema({
   },
 });
 
+//Admin Schema
 const adminSchema = new Schema({
   username: {
-    //mailId
+    type: String,
+    required: true,
+  },
+  email: {
     type: String,
     required: true,
   },
@@ -52,7 +52,77 @@ const adminSchema = new Schema({
   },
 });
 
+const QuestionSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+
+  sampleTestCase: {
+    input: { type: String, required: true },
+    output: { type: String, required: true },
+    isHidden: { type: Boolean, default: false },
+  },
+  hiddenTestCases: [
+    {
+      input: String,
+      output: String,
+      isHidden: { type: Boolean, default: true },
+    },
+  ],
+});
+
+const vivaSchema = new mongoose.Schema({
+  question: { type: String, required: true },
+  options: [
+    {
+      text: { type: String, required: true },
+      isCorrect: { type: Boolean, default: false },
+    },
+  ],
+});
+
+//CourseSchema
+const courseSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  questions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question",
+    },
+  ],
+  vivaQuestions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Viva",
+    },
+  ],
+});
+
+//Test Schema
+const TestSchema = new mongoose.Schema({
+  course: { type: String, required: true },
+  batch: { type: String, required: true },
+  branch: {
+    type: String,
+    required: true,
+  },
+  courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
+  year: { type: Number, required: true },
+  scheduledOn: { type: Date },
+  isCompleted: { type: Boolean, default: false },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
+});
+
+const resultSchema = new mongoose.Schema({
+  testId: { type: mongoose.Schema.Types.ObjectId, ref: "Test" },
+  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
+  codingScore: { type: Number, required: true },
+  vivaScore: { type: Number, required: true },
+});
+
 const User = mongoose.model("User", userSchema);
 const Admin = mongoose.model("Admin", adminSchema);
-
-module.exports = { User, Admin };
+const Question = mongoose.model("Question", QuestionSchema);
+const Test = mongoose.model("Test", TestSchema);
+const Course = mongoose.model("Course", courseSchema);
+const Viva = mongoose.model("Viva", vivaSchema);
+const Result = mongoose.model("Result", resultSchema);
+module.exports = { User, Admin, Question, Test, Course, Viva, Result };
