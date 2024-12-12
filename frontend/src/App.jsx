0e61f8ae498a81d2c8958993c93f7b089de6956b
Ranjan N <ranjann2004@gmail.com>
2024-12-12 19:09:40 +0530

    Added React router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/Landing";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
