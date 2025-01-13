fc3114cc83a76a1b00c50ced6b9785d416f50b14
Amogh <as7636688@gmail.com>
2025-01-13 23:21:49 +0530

    Updated user and admin middlewares

--- backend/middlewares/admin.js
+++ backend/middlewares/admin.js
@@ -1,16 +1,29 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const { Admin } = require("../db/index");
 
-function adminMiddleWare(req, res, next) {
+async function adminMiddleWare(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token)
+
+  if (!token) {
     return res.status(400).json({
       message: "No token provided",
     });
+  }
+
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.userId = decoded.userId;
+
+    const admin = await Admin.findById(req.userId);
+
+    if (!admin) {
+      return res.status(403).json({
+        message: "You are not authorized to access this resource. Admins only.",
+      });
+    }
+
     next();
   } catch (err) {
     res.status(403).json({
